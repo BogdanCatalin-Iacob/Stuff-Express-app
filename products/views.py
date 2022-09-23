d8ed97a29a4e556e730bf54b3d5bf1f89b2a7a1c
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-09-23 17:43:48 +0000

    Add sorting functionallity by price, rating, category

--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from .models import Product, Category
 
 
+# based on code institute django module
 def all_products(request):
     '''
     A view to show all products, including sorting and search queries
@@ -12,8 +13,27 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+
+        # query by sorting products
+        if 'sort' in request.GET:
+            sort_key = request.GET['sort']
+            sort = sort_key
+            # case insensitive sorting
+            if sort_key == 'name':
+                sort_key = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                # reverse sorting direction
+                if direction == 'desc':
+                    sort_key = f'-{sort_key}'
+            products = products.order_by(sort_key)
+
         # query for specific category by navbar menu
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -33,10 +53,13 @@ def all_products(request):
                 description__icontains=search_query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'curent_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
