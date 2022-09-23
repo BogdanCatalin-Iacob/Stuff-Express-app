bec80b44fa966eebd82f1fc7f2259d6feee687a4
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-09-23 15:36:51 +0000

    Add search functionality based on name or description

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -8,9 +10,23 @@ def all_products(request):
     '''
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'query' in request.GET:
+            search_query = request.GET['query']
+            if not search_query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            # search based on name or description
+            queries = Q(name__icontains=search_query) | Q(
+                description__icontains=search_query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
