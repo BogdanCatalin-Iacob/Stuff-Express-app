df714c6d574bc90583710b43cd0d1db299c56b1e
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-09-26 07:10:51 +0000

    Refator: curent_categories with current_categories in context

--- products/views.py
+++ products/views.py
@@ -58,7 +58,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'curent_categories': categories,
+        'current_categories': categories,
         'current_sorting': current_sorting,
     }
 
