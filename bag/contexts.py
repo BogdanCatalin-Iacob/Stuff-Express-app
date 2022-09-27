34e3952f6cd8fa99c76c6d0d6d629c1742f6c9e4
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-09-27 03:49:57 +0000

    Refactor: render individual quantity and subtotal

--- bag/contexts.py
+++ bag/contexts.py
@@ -18,11 +18,13 @@ def bag_contents(request):
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
         total += quantity * product.market_price
+        subtotal = quantity * product.market_price
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
+            'subtotal': subtotal,
         })
 
     # set delivery fee
