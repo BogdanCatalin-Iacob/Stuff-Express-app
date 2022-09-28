81d8c884d0c877a643783c3eb59101b6e164ae6b
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-09-28 02:13:28 +0000

    Add logic to add to bag items with sizes

--- bag/views.py
+++ bag/views.py
@@ -17,14 +17,30 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
+    # set the user selected size
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['size']
+
     # check if bag exist in session else create an empty dict
     bag = request.session.get('bag', {})
 
-    # set the item quantity
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    # add items to the bag with multiple sizes
+    if size:
+        # if the item already exist with the same size increase quantity
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'] += quantity
+            else:
+                bag[item_id]['items_by_size'] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        # set the item quantity if item has no sizes
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     # set the bag items into session
     request.session['bag'] = bag
