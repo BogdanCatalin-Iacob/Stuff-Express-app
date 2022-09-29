337aea0353f35a2348d5ad3706553f72f50677bc
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-09-29 22:18:54 +0000

    Add stripe public key and secret to context

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LRSjYGg4BpUsp5D5kJqxadDjW64t7xzSQshpH9UBEbzHHYK71gHmt2L6A6QNwrCWS3LMiZHEqdguKNnj8fV681P00dxo6Pztx',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
