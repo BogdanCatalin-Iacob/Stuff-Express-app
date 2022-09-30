92b76104294563f011bb3de2506fced0fc09890a
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-09-30 01:25:19 +0000

    Create payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -8,11 +8,17 @@ from bag.contexts import bag_contents
 import stripe
 
 
+# from code institute
 def checkout(request):
     '''
     Check if bag has items and
     create a new OrderForm instance
     '''
+
+    # get stripe keys
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There is nothing in your bag at the moment")
@@ -23,12 +29,24 @@ def checkout(request):
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
 
+    # create payment intent
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY
+            )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+        Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51LRSjYGg4BpUsp5D5kJqxadDjW64t7xzSQshpH9UBEbzHHYK71gHmt2L6A6QNwrCWS3LMiZHEqdguKNnj8fV681P00dxo6Pztx',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
