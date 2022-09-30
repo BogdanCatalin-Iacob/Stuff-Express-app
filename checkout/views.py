b85f95b9aa6946b8c4385dc96c703d4e24d65afe
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-09-30 00:57:57 +0000

    Get grand total from the bag context for stripe usage

--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render,redirect, reverse
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
+    '''
+    Check if bag has items and
+    create a new OrderForm instance
+    '''
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There is nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    # get the grand total from bag_contents
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
