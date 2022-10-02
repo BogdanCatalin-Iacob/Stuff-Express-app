41131c8210bb34a6deb325e85c53dd7507cbf3f3
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-10-02 10:00:03 +0000

    Create function order_history

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 def profile(request):
     '''
@@ -11,7 +13,7 @@ def profile(request):
     '''
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    if request.method =='POST':
+    if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
@@ -29,4 +31,21 @@ def profile(request):
         'on_profile_page': True,
     }
 
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True
+        }
     return render(request, template, context)
