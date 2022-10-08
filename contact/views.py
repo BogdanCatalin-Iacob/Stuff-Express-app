3f468fcd5499676fc1a932fdd426845c999a1802
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-10-08 19:37:44 +0000

    Add contact form POST logic

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render, reverse, HttpResponse
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
+
 from .forms import ContactForm
 
 
 def contact(request):
+    '''
+    Contact form to send an email to admin
+    '''
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Thank you! \
+                Your message was successfully sent.\
+                We'll get in touch soon!")
+            return redirect(reverse('products'))
+        else:
+            messages.error(request, 'Your message was not sent!\
+                Please, try again.')
+            return redirect(reverse('contact'))
 
     form = ContactForm()
 
@@ -11,4 +28,4 @@ def contact(request):
         'form': form,
     }
 
-    return render(request, template)
+    return render(request, template, context)
