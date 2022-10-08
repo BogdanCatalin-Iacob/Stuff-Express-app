9aa9b946dd001f147b72d56a39763beac8c3a984
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-10-08 20:39:47 +0000

    Add logic to form POST and send email

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, reverse, redirect
 from django.contrib import messages
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 from .forms import ContactForm
 
@@ -11,7 +14,19 @@ def contact(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()
+            email_data = form.save(commit=False)
+            # get email subject txt
+            email_subject = render_to_string(
+                'contact/confirmation_emails/confirmation_email_subject.txt',
+                {'email_data': email_data})
+            # get email body txt
+            email_body = render_to_string(
+                'contact/confirmation_emails/confirmation_email_body.txt',
+                {'email_data': email_data,
+                 'contact_email': settings.DEFAULT_FROM_EMAIL})
+            user_email = email_data.email
+            email_data.save()
+
             messages.success(request, "Thank you! \
                 Your message was successfully sent.\
                 We'll get in touch soon!")
@@ -20,6 +35,13 @@ def contact(request):
             messages.error(request, 'Your message was not sent!\
                 Please, try again.')
             return redirect(reverse('contact'))
+
+        # send email
+        send_mail(
+            email_subject,
+            email_body,
+            settings.DEFAULT_FROM_EMAIL,
+            [user_email])
 
     form = ContactForm()
 
