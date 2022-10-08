0a8fc4341d7840af299f578ed1622a80cd5020d2
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-10-08 18:02:23 +0000

    Refactor: names length

--- contact/models.py
+++ contact/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=254, null=False, blank=False)
-    last_name = models.CharField(max_length=254, null=False, blank=False)
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     message = models.TextField(max_length=3000, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
