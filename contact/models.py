975c6d007a30ab5a1e9b51a8f0a0a8184b78ccb9
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-10-08 16:32:47 +0000

    Create contact app

---
+++ contact/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=254, null=False, blank=False)
+    last_name = models.CharField(max_length=254, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    message = models.TextField(max_length=3000, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
