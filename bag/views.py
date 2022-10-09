0eb5cf2120466491f1f2cc91bb90bbe093522696
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-10-09 03:12:13 +0000

    Refactor: reduce lines length and remove extra empty lines

--- bag/views.py
+++ bag/views.py
@@ -38,18 +38,26 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+                messages.success(
+                    request,
+                    f'Updated size {size.upper()} {product.name} \
+                        quantity to {bag[item_id]["items_by_size"][size]}')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
-                messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
+                messages.success(
+                    request,
+                    f'Added size {size.upper()} {product.name} to your bag')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
+            messages.success(
+                request,
+                f'Added size {size.upper()} {product.name} to your bag')
     else:
         # set the item quantity if item has no sizes
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
-            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+            messages.success(
+                request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
@@ -82,17 +90,23 @@ def adjust_bag(request, item_id):
         # if the item already exist with the same size increase quantity
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
-            messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+            messages.success(
+                request,
+                f'Updated size {size.upper()} {product.name} \
+                    quantity to {bag[item_id]["items_by_size"][size]}')
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(
+                request,
+                f'Removed size {size.upper()} {product.name} from your bag')
     else:
         # update item quantity if item has no sizes
         if quantity > 0:
             bag[item_id] = quantity
-            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+            messages.success(
+                request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')
@@ -108,7 +122,6 @@ def remove_from_bag(request, item_id):
     '''
     Remove the item from bag
     '''
-
     try:
         product = get_object_or_404(Product, pk=item_id)
         # set the user selected size
@@ -124,7 +137,9 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(
+                request,
+                f'Removed size {size.upper()} {product.name} from your bag')
         else:
             # delete item if has no sizes
             bag.pop(item_id)
